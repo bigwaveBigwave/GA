139b58864fe2d277fa726011de8b6aec84f0fd85
bigwaveBigwave <gb6ke3@naver.com>
2023-05-24 23:59:28 +0900

    offspring 개수 변경

--- GA_1/GA_1.cpp
+++ GA_1/GA_1.cpp
@@ -80,7 +80,7 @@ std::vector<int> geneticAlgorithm(const std::vector<Edge>& edges, int numVertice
         }
 
         // Perform crossover and mutation to create new offspring
-        std::vector<std::vector<int>> offspring(populationSize);
+        std::vector<std::vector<int>> offspring(populationSize); 
         for (int i = 0; i < populationSize; i += 2) {
             std::uniform_real_distribution<int> randomIndex(0, numVertices / 2 - 1);
             // Perform crossover
